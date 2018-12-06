c674df6c40fa125abad6879e385171a3c77a6403
Maria Eugenia Inzaugarat <m.euge.inzaug@gmail.com>
2018-12-06 16:10:33 -0300

    Analysing Twitter

---
+++ .ipynb_checkpoints/twitter_credentials-checkpoint.py
@@ -0,0 +1,5 @@
+costumer_key = 'DuCgQPd03x882riJlV9yKPoDe'
+costumer_secret = '1tX3nC52xxlpb7D7UEHX1AHYaOZJ3O2KUikd6dg5a9Mz4ca044'
+
+access_token = '636714226-CqFMQjJNVnqWYzzkka5X1vhAgTB4i7U1MBtZHx1Z'
+access_secret = '4LuHbvfiB3Ywd8qVJ1e4SzGNOK1XHkfClnsa6DNL3eqag '
